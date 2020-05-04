af995d3e3d4bd8cae90e1b34b456e7f8f9f6e4f8
Damijan Kavs <damijan.kavs@comland.si>
2020-05-04 11:15:36 +0200

    added translation bradcrumbs

--- src/app/features/meter-units/modules/meter-units-routing.module.ts
+++ src/app/features/meter-units/modules/meter-units-routing.module.ts
@@ -53,6 +53,19 @@ const routes: Routes = [
 })
 export class MeterUnitsRoutingModule {
   constructor(private i18n: I18n) {
-    routes.forEach(x => (x.data.breadcrumb = i18n(x.data.breadcrumb)));
+    routes.map(x => {
+      x.children !== undefined
+        ? x.children.map(y => {
+            y.children !== undefined
+              ? y.children.map(z =>
+                  z.data.breadcrumb !== null ? (z.data.breadcrumb = i18n(z.data.breadcrumb)) : (z.data.breadcrumb = null)
+                )
+              : (y = y);
+            y.data.breadcrumb !== null ? (y.data.breadcrumb = i18n(y.data.breadcrumb)) : (y.data.breadcrumb = null);
+          })
+        : (x = x);
+      x.data.breadcrumb = i18n(x.data.breadcrumb);
+    });
+    // routes.forEach(x => (x.data.breadcrumb = i18n(x.data.breadcrumb)));
   }
 }
