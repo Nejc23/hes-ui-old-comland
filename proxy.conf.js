7ffc4002a5b29e4185830e5e43c9dbdfe47c809e
urosn <uros.nabernik@comland.si>
2020-09-09 14:23:19 +0200

    proxy config restored

--- proxy.conf.js
+++ proxy.conf.js
@@ -15,28 +15,28 @@ const PROXY_CONFIG = [
     context: [
       "/api/device-inventory-ui",
     ],
-    target: "http://10.20.8.49:5013",
+    target: "http://localhost:5013",
     secure: false
   },
   {
     context: [
       "/api/concentrator-inventory-ui",
     ],
-    target: "http://10.20.8.49:5012",
+    target: "http://localhost:5012",
     secure: false
   },
   {
     context: [
       "/api/scheduler",
     ],
-    target: "http://10.20.8.49:5009",
+    target: "http://localhost:5009",
     secure: false
   },
   {
     context: [
       "/api/crypto",
     ],
-    target: "http://10.20.8.49:5014",
+    target: "http://localhost:5014",
     secure: false
   },
 ]
