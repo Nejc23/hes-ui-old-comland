577c6965c373267240f945a0fb4aa3684a57be86
Damijan Kavs <damijan.kavs@comland.si>
2020-09-21 14:07:54 +0200

    added action buttons to the DCU and meter units grid

--- src/app/features/data-concentrator-units/components/grid-custom-components/grid-cell-actions.component.ts
+++ src/app/features/data-concentrator-units/components/grid-custom-components/grid-cell-actions.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { ICellRendererAngularComp } from '@ag-grid-community/angular';
+import { I18n } from '@ngx-translate/i18n-polyfill';
+import { GridApi } from '@ag-grid-community/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-grid-cell-name',
-  templateUrl: './grid-cell-name.component.html'
+  selector: 'app-grid-cell-actions',
+  templateUrl: './grid-cell-actions.component.html'
 })
-export class GridCellNameComponent implements ICellRendererAngularComp {
+export class GridCellActionsComponent implements ICellRendererAngularComp {
   public params: any;
+  public gridApi: GridApi;
 
-  constructor(private router: Router) {}
+  constructor(private i18n: I18n, private router: Router) {}
 
   // called on init
   agInit(params: any): void {
     this.params = params;
+    this.gridApi = this.params.api as GridApi;
   }
 
   // called when the cell is refreshed
   refresh(params: any): boolean {
-    this.params = params;
     return true;
   }
 
