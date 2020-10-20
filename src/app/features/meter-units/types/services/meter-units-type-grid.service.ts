9f5ad38f18f3204fde8caffa4207aaa740b64770
urosn <uros.nabernik@comland.si>
2020-10-20 15:56:35 +0200

    ciiState filter fix

--- src/app/features/meter-units/types/services/meter-units-type-grid.service.ts
+++ src/app/features/meter-units/types/services/meter-units-type-grid.service.ts
@@ -429,6 +429,7 @@ export class MeterUnitsTypeGridService {
       JSON.stringify(sessionFilter.readStatusFilter) === JSON.stringify(requestModel.readStatus) &&
       JSON.stringify(sessionFilter.firmwareFilter) === JSON.stringify(requestModel.firmware) &&
       JSON.stringify(sessionFilter.breakerStateFilter) === JSON.stringify(requestModel.disconnectorState) &&
+      JSON.stringify(sessionFilter.ciiStateFilter) === JSON.stringify(requestModel.ciiState) &&
       JSON.stringify(sessionFilter.showOnlyMeterUnitsWithMBusInfoFilter) === JSON.stringify(requestModel.showChildInfoMBus) &&
       JSON.stringify(sessionFilter.showDeletedMeterUnitsFilter) === JSON.stringify(requestModel.showDeleted) &&
       JSON.stringify(sessionFilter.showMeterUnitsWithoutTemplateFilter) === JSON.stringify(requestModel.showWithoutTemplate)
