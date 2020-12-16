086beefc6a30ceaefaafa18c45ecec6bdf9311d6
urosn <uros.nabernik@comland.si>
2020-12-16 16:22:06 +0100

    register selection fixed

--- src/app/features/meter-units/common/services/plc-meter-set-limiter.service.ts
+++ src/app/features/meter-units/common/services/plc-meter-set-limiter.service.ts
@@ -12,7 +12,7 @@ export class PlcMeterSetLimiterService {
 
   getListOfRegisterDefinitionNames(res: ResponseCommonRegisterGroup[]) {
     const registerDefList = res.map(
-      result2 => _.assign({ value: result2.name, id: result2.registerGroupId })
+      result2 => _.assign({ value: result2.name, id: result2.groupId })
       // result2.registerDefinitions.map(d => _.assign({ value: d.name, id: d.registerDefinitionId }))
     );
     // const nameList = _.flatMap(registerDefList, nameObj => _.map(nameObj, objVal => objVal));
