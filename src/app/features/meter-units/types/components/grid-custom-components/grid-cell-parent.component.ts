f46031f4ff78489c02c3b498936b60d9bd51f249
urosn <uros.nabernik@comland.si>
2020-12-24 12:55:28 +0100

    redirect from mu to dcu added

--- src/app/features/meter-units/types/components/grid-custom-components/grid-cell-parent.component.ts
+++ src/app/features/meter-units/types/components/grid-custom-components/grid-cell-parent.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ICellRendererAngularComp } from '@ag-grid-community/angular';
 import { MeterUnitsTypeStaticTextService } from '../../services/meter-units-type-static-text.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-grid-cell-parent',
@@ -10,7 +11,7 @@ export class GridCellParentComponent implements ICellRendererAngularComp {
   notAvailableText = this.statictextService.notAvailableTekst; // N/A
   public params: any;
 
-  constructor(private statictextService: MeterUnitsTypeStaticTextService) {}
+  constructor(private statictextService: MeterUnitsTypeStaticTextService, private router: Router) {}
   // called on init
   agInit(params: any): void {
     this.params = params;
@@ -20,5 +21,9 @@ export class GridCellParentComponent implements ICellRendererAngularComp {
   refresh(params: any): boolean {
     this.params = params;
     return true;
+  }
+
+  openDcuDetails() {
+    this.router.navigate([`/dataConcentratorUnits/${this.params.data.concentratorId}`]);
   }
 }
