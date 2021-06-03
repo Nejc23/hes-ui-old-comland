b1ebccbafb91bb06f7a407936273709ac73e10d4
Nejc Princic <nejc.princic@enerdat.com>
2021-06-03 15:21:04 +0200

    fix(PNTLNK-1733): Old reading date fix

--- src/app/features/meter-units/types/components/grid-custom-components/grid-cell-threshold.component.ts
+++ src/app/features/meter-units/types/components/grid-custom-components/grid-cell-threshold.component.ts
@@ -3,6 +3,7 @@ import { ICellRendererAngularComp } from '@ag-grid-community/angular';
 import { MeterUnitsTypeStaticTextService } from '../../services/meter-units-type-static-text.service';
 import * as moment from 'moment';
 import { environment } from 'src/environments/environment';
+import { dateServerFormat } from '../../../../../shared/forms/consts/date-format';
 
 @Component({
   selector: 'app-grid-cell-threshold',
@@ -18,6 +19,7 @@ export class GridCellThresholdComponent implements ICellRendererAngularComp {
   thresholdValue = this.notAvailableText;
 
   constructor(private staticTextService: MeterUnitsTypeStaticTextService) {}
+
   // called on init
   agInit(params: any): void {
     this.params = params;
@@ -40,14 +42,14 @@ export class GridCellThresholdComponent implements ICellRendererAngularComp {
   // called when the cell is refreshed
   refresh(params: any): boolean {
     this.params = params;
+    this.checkDate();
     return true;
   }
 
   checkDate() {
-    //TODO CHECK DATE FORMAT WITH BE
     if (
       this.params.value?.timestamp &&
-      moment(this.params.value.timestamp, 'YYYY-MM-DD hh:mm:ss') < moment().subtract(this.preconfiguredThreshold, 'day')
+      moment(this.params.value.timestamp, dateServerFormat) < moment().subtract(this.preconfiguredThreshold, 'day')
     ) {
       this.oldReadingDate = true;
     }
