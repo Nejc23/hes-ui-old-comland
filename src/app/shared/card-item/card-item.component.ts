5bb01756b0c683b3f6a3459d49bc2a2ab2cf40b8
Nejc Princic <nejc.princic@enerdat.com>
2021-07-29 11:37:25 +0200

    Card item dynamic form redner

--- src/app/shared/card-item/card-item.component.ts
+++ src/app/shared/card-item/card-item.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+
+export interface IFormData {
+  name: string;
+  control: AbstractControl;
+}
 
 @Component({
   selector: 'app-card-item',
   templateUrl: './card-item.component.html',
   styleUrls: ['./card-item.component.scss']
 })
-export class CardItemComponent implements OnInit {
+export class CardItemComponent implements OnInit, OnChanges {
+  @Input() withEdit = false;
+  @Input() form: FormGroup;
+  controls: Array<IFormData> = [];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges() {
+    if (this.form) {
+      debugger;
+      Object.keys(this.form.controls).forEach((control: string) => {
+        debugger;
+        const typedControl: AbstractControl = this.form.controls[control];
+        this.controls.push({
+          name: control,
+          control: typedControl
+        });
+        console.log(typedControl);
+        // should log the form controls value and be typed correctly
+      });
+    }
+  }
 }
