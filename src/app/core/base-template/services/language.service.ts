be8d57a0ba8b7e979c1fe9074f6a59a6ac4f3d00
Tadej Lokar <tadej.lokar@enerdat.com>
2021-08-10 13:22:04 +0000

    fix(HES-118): On language change all strings should be localized to a newly selected language

--- src/app/core/base-template/services/language.service.ts
+++ src/app/core/base-template/services/language.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { Language, languages } from 'src/environments/config';
 import { CldrIntlService, IntlService } from '@progress/kendo-angular-intl';
+import { Language, languages } from 'src/environments/config';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,9 @@ import { CldrIntlService, IntlService } from '@progress/kendo-angular-intl';
 export class LanguageService {
   public selectedLang: Language;
 
-  constructor(private translate: TranslateService, public intlService: IntlService) {}
+  constructor(private translate: TranslateService, public intlService: IntlService, private router: Router) {
+    this.translate.onLangChange.subscribe((x) => this.refreshComponents());
+  }
 
   selectLang(lang: string) {
     if (lang !== this.selectedLang?.id) {
@@ -30,5 +33,12 @@ export class LanguageService {
       return languages.find((lng) => lng.id == 'en');
     }
     return languages.find((lng) => lng.id == lang);
+  }
+
+  private refreshComponents(): void {
+    const prev = this.router.url;
+    this.router.navigate(['/language-switch']).then((data) => {
+      this.router.navigate([prev]);
+    });
   }
 }
