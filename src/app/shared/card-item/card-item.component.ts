8d807c7b2368c08ad395ac31aef84a86f17e411d
Nejc Princic <nejc.princic@enerdat.com>
2021-08-23 14:42:48 +0200

    Openstreetmap replaced with Mapbox

--- src/app/shared/card-item/card-item.component.ts
+++ src/app/shared/card-item/card-item.component.ts
@@ -37,6 +37,7 @@ export class CardItemComponent implements OnInit, OnChanges {
   map: any;
   marker: any;
   options: any;
+  accessToken = 'pk.eyJ1IjoibWJ1bG92ZWMiLCJhIjoiY2tzb2tqenNsMG54YTJwbzZ3M3JmcW1xOCJ9.Yvtz-kegMWE4_uQuXjDxQA';
 
   meterStatusGraphColors = [
     {
@@ -87,7 +88,18 @@ export class CardItemComponent implements OnInit, OnChanges {
       });
 
       this.options = {
-        layers: [tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 }), this.marker],
+        layers: [
+          tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: this.accessToken
+          }),
+          this.marker
+        ],
         zoom: 13,
         center: latLng(this.latLang[0], this.latLang[1])
       };
