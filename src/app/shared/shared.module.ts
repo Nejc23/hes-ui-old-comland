d618f16799987f197dc0b2732e38fbbfc53ea5bc
Nejc Princic <nejc.princic@enerdat.com>
2021-08-23 09:24:17 +0200

    NgxChartsModule removed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -168,8 +168,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     ActiveJobsListComponent,
     PageTitleComponent,
     CardItemComponent,
-    MiniCardItemComponent,
-    NgxChartsModule
+    MiniCardItemComponent
   ],
   imports: [
     CommonModule,
