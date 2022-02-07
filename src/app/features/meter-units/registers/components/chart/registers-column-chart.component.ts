9cb4da99bfb6a362517fa8b100d08643b2cc0e1b
Nejc Princic <nejc.princic@enerdat.com>
2022-02-07 13:20:58 +0100

    fix(HES-830): Value 0 is not visible on grid fix(HES-740): Registers chart fix

--- src/app/features/meter-units/registers/components/chart/registers-column-chart.component.ts
+++ src/app/features/meter-units/registers/components/chart/registers-column-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { EventsByTimestamp } from '../../interfaces/events-processing.interface';
 import * as moment from 'moment';
@@ -8,11 +8,12 @@ import * as _ from 'lodash';
   templateUrl: 'registers-column-chart.component.html',
   selector: 'app-registers-column-chart'
 })
-export class RegistersColumnChartComponent implements OnInit {
+export class RegistersColumnChartComponent implements OnChanges {
   @Input() eventsByTimestamp: EventsByTimestamp[] = [];
   @Input() title = '';
   @Input() hours = false;
   formattedEvents;
+  formatted = false;
 
   public dateFormats;
   public culture = 'de-DE'; // en-US for am/PM
@@ -22,13 +23,14 @@ export class RegistersColumnChartComponent implements OnInit {
     this.culture = environment.kendoChartCulture;
   }
 
-  ngOnInit(): void {
-    if (!this.hours) {
+  ngOnChanges(): void {
+    if (this.eventsByTimestamp && !this.hours && !this.formatted) {
       this.formattedEvents = _.cloneDeep(this.eventsByTimestamp);
       // format for days with timezone due to kendo chart wrong date display
       this.formattedEvents.forEach((event) => {
         event.timestamp = moment(event.timestamp).format(environment.dateOnlyFormat);
       });
+      this.formatted = true;
     }
   }
 }
