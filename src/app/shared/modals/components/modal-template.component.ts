386027adbd849685398d83a44760c668877413d4
Nejc Prinčič <nejc.princic@enerdat.com>
2022-04-14 12:40:05 +0000

    feat(HES-1022): Data export reports

--- src/app/shared/modals/components/modal-template.component.ts
+++ src/app/shared/modals/components/modal-template.component.ts
@@ -1,11 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal-template',
   templateUrl: './modal-template.component.html'
 })
 export class ModalTemplateComponent {
+  @Input() loading = false;
   @Output() dismiss = new EventEmitter();
+
   constructor() {}
 
   close() {
